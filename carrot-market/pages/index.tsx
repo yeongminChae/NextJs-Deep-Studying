2fbbbd3053ac4cb01c9e9c81d6f069a67c81da88
Yeongmin_Chae <codudals98@naver.com>
2022-08-01 22:37:54 +0900

    tailwind more modifier

--- carrot-market/pages/index.tsx
+++ carrot-market/pages/index.tsx
@@ -2,24 +2,29 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <form className="flex flex-col space-y-2 p-5 ">
+    <div className="flex flex-col space-y-2 p-5">
+      <details className="select-none open:text-white open:bg-indigo-400 ">
+        <summary className=" cursor-pointer">What is my fav food?</summary>
+        <span>kimchi</span>
+      </details>
+
+      <ul className="list-decimal marker:text-teal-500">
+        <li>Hi</li>
+        <li>Hi</li>
+        <li>Hi</li>
+      </ul>
+
       <input
-        type="text"
-        required
-        placeholder=" Username"
-        className="border p-1 peer border-gray-400 rounded-md"
+        type="file"
+        className="file:hover:text-purple-400 file:hover:bg-white file:hover:border-purple-400 
+        file:transition-colors file:cursor-pointer file:hover:border file:border-0 
+        file:rounded-xl file:px-5 file:text-white file:bg-purple-400 "
       />
-      <span className="hidden peer-invalid:block peer-invalid:text-red-500">
-        This input is invalid
-      </span>
-      <span className="hidden peer-valid:block peer-valid:text-teal-500">
-        Awosome Username
-      </span>
-      <span className="hidden peer-hover:block peer-valid:text-amber-500">
-        Hello
-      </span>
-      <input type="submit" value="login" className="bg-white"></input>
-    </form>
+
+      <p className="first-letter:text-7xl first-letter:hover:text-purple-400  ">
+        make something happened
+      </p>
+    </div>
   );
 };
 
