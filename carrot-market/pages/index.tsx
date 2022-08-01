3059502d9470b3cb668a28d99be4ff5fb7dcfdf1
Yeongmin_Chae <codudals98@naver.com>
2022-08-01 18:39:08 +0900

    tailwind transition

--- carrot-market/pages/index.tsx
+++ carrot-market/pages/index.tsx
@@ -62,9 +62,9 @@ const Home: NextPage = () => {
           <span className="text-xs text-gray-500">Chair</span>
           <div className="mt-3 mb-5 flex justify-between items-center">
             <div className="space-x-2">
-              <button className="w-5 h-5 rounded-full bg-yellow-300"></button>
-              <button className="w-5 h-5 rounded-full bg-indigo-500"></button>
-              <button className="w-5 h-5 rounded-full bg-teal-500"></button>
+              <button className="w-5 h-5 rounded-full bg-yellow-300 focus:ring-2 ring-offset-2 ring-yellow-400 transition "></button>
+              <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-400 transition "></button>
+              <button className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2 ring-teal-400 transition "></button>
             </div>
             <div className="flex items-center space-x-5 ">
               <button className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
