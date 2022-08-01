7202298de2c45142e716d26cf40835f4fcd6b495
Yeongmin_Chae <codudals98@naver.com>
2022-08-01 18:28:51 +0900

    tailwind modifier

--- carrot-market/pages/index.tsx
+++ carrot-market/pages/index.tsx
@@ -17,9 +17,14 @@ const Home: NextPage = () => {
           <span>Total</span>
           <span className="font-semibold">$10</span>
         </div>
-        <div className="mt-5 text-white p-3 bg-blue-500 text-center rounded-xl w-2/4 mx-auto ">
+        <button
+          className="mt-5 text-white p-3 bg-blue-500 text-center rounded-xl w-2/4 mx-auto first-letter 
+        hover:bg-teal-500 hover:text-black
+        active:bg-yellow-300 focus:bg-red-500
+        "
+        >
           Checkout
-        </div>
+        </button>
       </div>
       <div className="bg-white overflow-hidden rounded-3xl shadow-xl">
         <div className="bg-blue-500 p-6 pb-14 ">
@@ -56,10 +61,10 @@ const Home: NextPage = () => {
           <span className="font-medium mb-1.5 text-xl ">Swoon Lounge</span>
           <span className="text-xs text-gray-500">Chair</span>
           <div className="mt-3 mb-5 flex justify-between items-center">
-            <div>
-              <input type="radio"></input>
-              <input type="radio"></input>
-              <input type="radio"></input>
+            <div className="space-x-2">
+              <button className="w-5 h-5 rounded-full bg-yellow-300"></button>
+              <button className="w-5 h-5 rounded-full bg-indigo-500"></button>
+              <button className="w-5 h-5 rounded-full bg-teal-500"></button>
             </div>
             <div className="flex items-center space-x-5 ">
               <button className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
