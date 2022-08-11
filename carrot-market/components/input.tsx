93b29f312e706bdcffe6d2fde8b8b05c011b5291
Yeongmin_Chae <codudals98@naver.com>
2022-08-11 23:05:18 +0900

    chapter 6 : Database Setup (Prisma) < prisma is ORM(Object Relating Mapping) for typescript and Node.js which mean that is kinda translator between node.js and typescript's database >

--- carrot-market/components/input.tsx
+++ carrot-market/components/input.tsx
@@ -33,7 +33,7 @@ export default function Input({
           <input
             id={name}
             {...rest}
-            className="appearance-none pl-7 w-full px-3 py-2  border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+            className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
           />
           <div className="absolute right-0 pr-3 flex items-center pointer-events-none ">
             <span className="text-gray-500">USD</span>
