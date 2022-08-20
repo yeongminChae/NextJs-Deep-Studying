dcba6e0d4d603a3c6286c0c136f715a7e3fddbe3
Yeongmin_Chae <codudals98@naver.com>
2022-08-20 15:29:21 +0900

    chapter 11 : Products -> Product Detail page

--- carrot-market/pages/products/[id].tsx
+++ carrot-market/pages/products/[id].tsx
@@ -1,41 +1,46 @@
 import type { NextPage } from "next";
-import Link from "next/link";
 import Button from "@components/button";
 import Layout from "@components/layout";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import Link from "next/link";
 
 const ItemDetail: NextPage = () => {
+  const router = useRouter();
+  const { data, error } = useSWR(
+    router.query.id ? `/api/products/${router.query.id}` : null
+  );
   return (
     <Layout canGoBack>
-      <div className="px-4 py-10">
+      <div className="px-4  py-4">
         <div className="mb-8">
           <div className="h-96 bg-slate-300" />
-          <div className="py-3 cursor-pointer border-t border-b flex items-center space-x-3">
+          <div className="flex cursor-pointer py-3 border-t border-b items-center space-x-3">
             <div className="w-12 h-12 rounded-full bg-slate-300" />
             <div>
-              <p className="text-sm font-medium text-gray-700">Steve Jebs</p>{" "}
-              <Link href="/profile">
-                <p className="text-xs font-medium cursor-pointer text-gray-500">
+              <p className="text-sm font-medium text-gray-700">
+                {data?.product?.User?.name}
+              </p>
+              <Link href={`/users/profiles/${data?.product?.User?.id}`}>
+                {/* if i want to use username making a new field in the model
+                 also making a new form in enter form which uses for making account */}
+                <a className="text-xs font-medium text-gray-500">
                   View profile &rarr;
-                </p>
+                </a>
               </Link>
             </div>
           </div>
-          <div className="mt-10">
-            <h1 className="text-2xl font-bold text-gray-900">Galaxy S50</h1>{" "}
-            <span className="text-3xl mt-3 text-gray-900">$140</span>
-            <p className="text-base my-6 text-gray-700">
-              My money&apos;s in that office, right? If she start giving me some
-              bullshit about it ain&apos;t there, and we got to go someplace
-              else and get it, I&apos;m gonna shoot you in the head then and
-              there. Then I&apos;m gonna shoot that bitch in the kneecaps, find
-              out where my goddamn money is. She gonna tell me too. Hey, look at
-              me when I&apos;m talking to you, motherfucker. You listen: we go
-              in there, and that ni**a Winston or anybody else is in there, you
-              the first motherfucker to get shot. You understand?
-            </p>
+          <div className="mt-5">
+            <h1 className="text-3xl font-bold text-gray-900">
+              {data?.product?.name}
+            </h1>
+            <span className="text-2xl block mt-3 text-gray-900">
+              ${data?.product?.price}
+            </span>
+            <p className=" my-6 text-gray-700">{data?.product?.description}</p>
             <div className="flex items-center justify-between space-x-2">
-              <Button text="Talk to seller" />
-              <button className="p-3 flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500 rounded-md  ">
+              <Button large text="Talk to seller" />
+              <button className="p-3 rounded-md flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500">
                 <svg
                   className="h-6 w-6 "
                   xmlns="http://www.w3.org/2000/svg"
@@ -57,12 +62,12 @@ const ItemDetail: NextPage = () => {
         </div>
         <div>
           <h2 className="text-2xl font-bold text-gray-900">Similar items</h2>
-          <div className="mt-6 grid grid-cols-2 gap-4">
+          <div className=" mt-6 grid grid-cols-2 gap-4">
             {[1, 2, 3, 4, 5, 6].map((_, i) => (
               <div key={i}>
-                <div className="h-56 w-full mb-4 bg-slate-200" />
+                <div className="h-56 w-full mb-4 bg-slate-300" />
                 <h3 className="text-gray-700 -mb-1">Galaxy S60</h3>
-                <p className="text-sm font-medium text-gray-900">$6</p>
+                <span className="text-sm font-medium text-gray-900">$6</span>
               </div>
             ))}
           </div>
@@ -71,4 +76,5 @@ const ItemDetail: NextPage = () => {
     </Layout>
   );
 };
+
 export default ItemDetail;
