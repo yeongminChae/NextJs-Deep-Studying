6d8451c4d1cd40f54de30f44cd49457509c20f74
Yeongmin_Chae <codudals98@naver.com>
2022-08-21 10:25:06 +0900

    chapter 12 : 동네생활 -> Forms and Handler

--- carrot-market/pages/community/write.tsx
+++ carrot-market/pages/community/write.tsx
@@ -2,13 +2,43 @@ import type { NextPage } from "next";
 import Layout from "@components/layout";
 import Button from "@components/button";
 import TextArea from "@components/textarea";
+import { useForm } from "react-hook-form";
+import useMutation from "@libs/cleint/useMutation";
+import { useEffect } from "react";
+import { Post } from "@prisma/client";
+import { useRouter } from "next/router";
+
+interface WriteForm {
+  question: string;
+}
+
+interface WriteResponse {
+  ok: boolean;
+  post: Post;
+}
 
 const Write: NextPage = () => {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<WriteForm>();
+  const [post, { loading, data }] = useMutation("/api/posts");
+  const onValid = (data: WriteForm) => {
+    if (loading) return; // it's for preventing when users click the button too many times
+    post(data);
+  };
+  useEffect(() => {
+    if (data && data.ok) {
+      router.push(`/community/${data.post.id} `);
+    }
+  }, [data, router]);
   return (
     <Layout canGoBack>
-      <form className="px-4 py-10">
-        <TextArea required placeholder="Answer this question!" />
-        <Button text="Submit" />
+      <form onSubmit={handleSubmit(onValid)} className="px-4 py-10">
+        <TextArea
+          register={register("question", { required: true, minLength: 5 })}
+          required
+          placeholder="Answer this question!"
+        />
+        <Button text={loading ? "Loading..." : "Submit"} />
       </form>
     </Layout>
   );
