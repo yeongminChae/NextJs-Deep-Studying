14a7a337db8e9dfd8d656208df509144b94c1c2a
Yeongmin_Chae <codudals98@naver.com>
2022-08-29 16:29:49 +0900

    chapter 15 : Cloudflare Images -> CloudFlare setup

---
+++ carrot-market/pages/api/files.ts
@@ -0,0 +1,21 @@
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  res.json({
+    ok: true,
+    ure: "",
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["POST"],
+    handler,
+  })
+);
