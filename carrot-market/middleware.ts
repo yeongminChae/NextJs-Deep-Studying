ad0bf82df375b005339708812f89ac969306741e
Yeongmin_Chae <codudals98@naver.com>
2022-09-10 17:42:21 +0900

    NextJs : middleware(response and redirect)

--- carrot-market/middleware.ts
+++ carrot-market/middleware.ts
@@ -1,8 +1,21 @@
+import { userAgent, NextResponse } from "next/server";
 import type { NextRequest, NextFetchEvent } from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  console.log("it works! global middleware");
-  if (req.nextUrl.pathname.startsWith("/chats")) {
-    console.log("it works! chat middleware");
+  // if (req.nextUxrl.pathname.startsWith("/chats")) {
+  //   console.log("chats Only middleware");
+  // }
+  if (req.nextUrl.pathname.startsWith("/")) {
+    const ua = userAgent(req);
+    if (ua?.isBot) {
+      return new Response("Plz don't be a bot. Be human.", { status: 403 });
+    }
   }
+  if (req.nextUrl.pathname.startsWith("/api")) {
+    if (!req.url.includes("/enter") && !req.cookies.get("carrotsession")) {
+      console.log("carrotsession");
+      NextResponse.redirect(`${req.nextUrl.origin}/enter`);
+    }
+  }
+  return NextResponse.next();
 }
