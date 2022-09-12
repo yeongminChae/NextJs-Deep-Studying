fd44e72fa692fb8846fcbeb97f592a7d24fe78ef
Yeongmin_Chae <codudals98@naver.com>
2022-09-12 18:36:48 +0900

    NextJs : How to fetch in nextJs ( second : getStaticProps ) => introduce about Blog section

---
+++ carrot-market/pages/blog.tsx
@@ -0,0 +1,12 @@
+import Layout from "@components/layout";
+
+export default function Blog() {
+  return (
+    <Layout title="Blog" seoTitle="Blog">
+      <h1 className="font-semibold text-lg">Latest Posts : </h1>
+      <ul>
+        <li>Welcome everyone!</li>
+      </ul>
+    </Layout>
+  );
+}
---
+++ carrot-market/posts/01-first-post.md
@@ -0,0 +1,4 @@
+# Welcome everyone!
+
+This is my first blog post!
+thank u for reading!
