16a97c7763d2e6078c149a4bad09a0c51ef5c394
Yeongmin_Chae <codudals98@naver.com>
2022-09-14 23:27:20 +0900

    Animation - Framer Motion : Variants part 1

--- react-masterclass-tailwind/pages/index.tsx
+++ react-masterclass-tailwind/pages/index.tsx
@@ -1,25 +1,18 @@
 import type { NextPage } from "next";
 import { motion } from "framer-motion";
 
+const myVars = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+};
+
 const Home: NextPage = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-gradient-to-tl from-purple-600 to-pink-600">
-      {/* <motion.div
-        animate={{ borderRadius: "100px" }}
-        // transition={{ delay: 3 }}
-        transition={{ duration: 3 }}
-        className="h-48 w-48 rounded-lg bg-white shadow-md"
-       />  basic framer motion  */}
-
       <motion.div
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, rotateZ: 360 }}
-        // transition={{ type: "tween" }}
-        // transition={{ type: "spring" }} // basic type , which is jumping
-        transition={{ type: "spring", delay: 0.5 }}
-        // transition={{ type: "spring", bounce: 0.5 }} //  more bouncing
-        // transition={{ type: "spring", stiffness: 10 }} // make smth stiff
-        // transition={{ type: "spring", damping: 10 }} // more jumping
+        variants={myVars}
+        initial="start"
+        animate="end"
         className="h-48 w-48 rounded-2xl bg-white shadow-md"
       ></motion.div>
     </div>
