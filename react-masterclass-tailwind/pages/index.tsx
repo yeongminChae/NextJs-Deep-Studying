1113dcb1baba1c77c3fb1e8ff6cab4193ed05521
Yeongmin_Chae <codudals98@naver.com>
2022-09-14 23:48:25 +0900

    Animation - Framer Motion : Variants part 2

--- react-masterclass-tailwind/pages/index.tsx
+++ react-masterclass-tailwind/pages/index.tsx
@@ -1,20 +1,58 @@
 import type { NextPage } from "next";
 import { motion } from "framer-motion";
 
-const myVars = {
-  start: { scale: 0 },
-  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+const boxVars = {
+  start: { opacity: 0, scale: 0.5 },
+  end: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.3,
+    },
+  },
+};
+
+const circleVars = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
 };
 
 const Home: NextPage = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-gradient-to-tl from-purple-600 to-pink-600">
       <motion.div
-        variants={myVars}
+        variants={boxVars}
         initial="start"
         animate="end"
-        className="h-48 w-48 rounded-2xl bg-white shadow-md"
-      ></motion.div>
+        className="grid h-44 w-44 grid-cols-2 rounded-2xl bg-white/[0.1] shadow-md "
+      >
+        <motion.div
+          variants={circleVars}
+          className="h-16 w-16 place-self-center rounded-full bg-white shadow-md"
+        />
+        <motion.div
+          variants={circleVars}
+          className="h-16 w-16 place-self-center rounded-full bg-white shadow-md"
+        />
+        <motion.div
+          variants={circleVars}
+          className="h-16 w-16 place-self-center rounded-full bg-white shadow-md"
+        />
+        <motion.div
+          variants={circleVars}
+          className="h-16 w-16 place-self-center rounded-full bg-white shadow-md"
+        />
+      </motion.div>
     </div>
   );
 };
