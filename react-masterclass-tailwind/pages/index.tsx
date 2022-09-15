778ff33ada34b163a68f0c6a656ed13dc629ae25
Yeongmin_Chae <codudals98@naver.com>
2022-09-15 10:08:05 +0900

    Animation - Framer Motion : Gesture part 2

--- react-masterclass-tailwind/pages/index.tsx
+++ react-masterclass-tailwind/pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from "next";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const boxVars = {
   hover: { scale: 1.5, rotateZ: 90 },
   click: { scale: 1, borderRadius: "100px" },
   drag: {
-    backgroundColor: "rgb(191, 172, 224)", // if i put rgb(int) then motion will adapte animatiion automatically
+    backgroundColor: "rgb(191, 172, 224)",
     transition: {
       duration: 3,
     },
@@ -13,16 +14,25 @@ const boxVars = {
 };
 
 const Home: NextPage = () => {
+  const biggerBoxref = useRef<HTMLDivElement>(null);
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-gradient-to-tl from-purple-600 to-pink-600">
       <motion.div
-        variants={boxVars}
-        drag
-        whileDrag="drag"
-        whileHover="hover"
-        whileTap="click"
-        className="grid h-44 w-44 grid-cols-2 rounded-2xl bg-white shadow-md "
-      ></motion.div>
+        ref={biggerBoxref}
+        className="flex h-96 w-96 items-center justify-center  rounded-2xl bg-white/[0.2] "
+      >
+        <motion.div
+          variants={boxVars}
+          drag
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxref}
+          whileDrag="drag"
+          whileHover="hover"
+          whileTap="click"
+          className="grid h-32 w-32 grid-cols-2 rounded-2xl bg-white shadow-md "
+        />
+      </motion.div>
     </div>
   );
 };
