567f6f68ddd1b0227d52b0d774685d9379e18304
Yeongmin_Chae <codudals98@naver.com>
2022-09-15 09:43:59 +0900

    Animation - Framer Motion : Gesture part 1

--- react-masterclass-tailwind/pages/index.tsx
+++ react-masterclass-tailwind/pages/index.tsx
@@ -2,28 +2,13 @@ import type { NextPage } from "next";
 import { motion } from "framer-motion";
 
 const boxVars = {
-  start: { opacity: 0, scale: 0.5 },
-  end: {
-    scale: 1,
-    opacity: 1,
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { scale: 1, borderRadius: "100px" },
+  drag: {
+    backgroundColor: "rgb(191, 172, 224)", // if i put rgb(int) then motion will adapte animatiion automatically
     transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.3,
+      duration: 3,
     },
-  },
-};
-
-const circleVars = {
-  start: {
-    opacity: 0,
-    y: 10,
-  },
-  end: {
-    opacity: 1,
-    y: 0,
   },
 };
 
@@ -32,27 +17,12 @@ const Home: NextPage = () => {
     <div className="flex h-screen w-screen items-center justify-center bg-gradient-to-tl from-purple-600 to-pink-600">
       <motion.div
         variants={boxVars}
-        initial="start"
-        animate="end"
-        className="grid h-44 w-44 grid-cols-2 rounded-2xl bg-white/[0.1] shadow-md "
-      >
-        <motion.div
-          variants={circleVars}
-          className="h-16 w-16 place-self-center rounded-full bg-white shadow-md"
-        />
-        <motion.div
-          variants={circleVars}
-          className="h-16 w-16 place-self-center rounded-full bg-white shadow-md"
-        />
-        <motion.div
-          variants={circleVars}
-          className="h-16 w-16 place-self-center rounded-full bg-white shadow-md"
-        />
-        <motion.div
-          variants={circleVars}
-          className="h-16 w-16 place-self-center rounded-full bg-white shadow-md"
-        />
-      </motion.div>
+        drag
+        whileDrag="drag"
+        whileHover="hover"
+        whileTap="click"
+        className="grid h-44 w-44 grid-cols-2 rounded-2xl bg-white shadow-md "
+      ></motion.div>
     </div>
   );
 };
