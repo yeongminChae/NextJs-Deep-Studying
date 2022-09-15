713cbabf3e6e5101dfdd8664f572df6b12ec4794
Yeongmin_Chae <codudals98@naver.com>
2022-09-15 10:43:20 +0900

    Animation - Framer Motion : Motion Value part 1&2

--- react-masterclass-tailwind/pages/index.tsx
+++ react-masterclass-tailwind/pages/index.tsx
@@ -1,38 +1,22 @@
 import type { NextPage } from "next";
-import { motion } from "framer-motion";
-import { useRef } from "react";
-
-const boxVars = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: "100px" },
-  drag: {
-    backgroundColor: "rgb(191, 172, 224)",
-    transition: {
-      duration: 3,
-    },
-  },
-};
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
-  const biggerBoxref = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
+  useEffect(() => {
+    // x.onChange(()=>console.log(x.get()))
+    scale.onChange(() => console.log(scale.get()));
+  }, [x, scale]);
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-gradient-to-tl from-purple-600 to-pink-600">
       <motion.div
-        ref={biggerBoxref}
-        className="flex h-96 w-96 items-center justify-center  rounded-2xl bg-white/[0.2] "
-      >
-        <motion.div
-          variants={boxVars}
-          drag
-          dragSnapToOrigin
-          dragElastic={0.5}
-          dragConstraints={biggerBoxref}
-          whileDrag="drag"
-          whileHover="hover"
-          whileTap="click"
-          className="grid h-32 w-32 grid-cols-2 rounded-2xl bg-white shadow-md "
-        />
-      </motion.div>
+        drag="x"
+        dragSnapToOrigin
+        style={{ x, scale: scale }}
+        className="grid h-32 w-32 grid-cols-2 rounded-2xl bg-white shadow-md "
+      />
     </div>
   );
 };
