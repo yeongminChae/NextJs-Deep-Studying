b32ebf3af65db28eb0e3969e2b4782724cf3b290
Yeongmin_Chae <codudals98@naver.com>
2022-09-15 18:01:48 +0900

    Animation - Framer Motion : SVG animation

--- react-masterclass-tailwind/pages/index.tsx
+++ react-masterclass-tailwind/pages/index.tsx
@@ -1,37 +1,41 @@
 import type { NextPage } from "next";
-import { motion, useMotionValue, useTransform, useScroll } from "framer-motion";
-import { useEffect } from "react";
+import { motion } from "framer-motion";
+
+const svg = {
+  start: {
+    fill: "rgba(255,255,255,0",
+    pathLength: 0,
+  },
+  end: {
+    fill: "rgba(255,255,255,1",
+    pathLength: 1,
+    // transition: { duration: 3 }, do the same time fill the inside and stroke
+  },
+};
 
 const Home: NextPage = () => {
-  const x = useMotionValue(0);
-  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
-  const gradient = useTransform(
-    x,
-    [-800, 0, 800],
-    [
-      "linear-gradient(135deg,rgb(0, 238, 214),rgb(18, 70, 240))",
-      "linear-gradient(135deg,rgb(160, 132, 202),rgb(235, 199, 232))",
-      "linear-gradient(135deg,rgb(247, 246, 220),rgb(255, 192, 144))",
-    ]
-  );
-  const { scrollY, scrollYProgress } = useScroll();
-  // useEffect(() => {
-  //   scrollY.onChange(() => {
-  //     console.log(scrollY.get(), scrollYProgress.get());
-  //   });
-  // }, [scrollY, scrollYProgress]);
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
   return (
-    <motion.div
-      style={{ background: gradient }}
-      className="flex h-[200vh] w-screen items-center justify-center bg-gradient-to-tl from-purple-600 to-pink-600"
-    >
-      <motion.div
-        drag="x"
-        dragSnapToOrigin
-        style={{ x, rotateZ, scale }}
-        className="bg-white[/0.2] grid h-32 w-32 grid-cols-2 rounded-2xl border-2 border-double border-white shadow-md  "
-      />
+    <motion.div className="flex h-screen w-screen items-center justify-center bg-gradient-to-tl from-purple-600 to-pink-600">
+      <motion.svg
+        focusable="false"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 448 512"
+        className="h-48 w-48"
+      >
+        <motion.path
+          variants={svg}
+          initial="start"
+          animate="end"
+          transition={{
+            default: { duration: 5 },
+            fill: { duration: 2, delay: 3 },
+          }} // animate fill and stroke at not a same time
+          stroke="white"
+          strokeWidth="2"
+          fill="transparent"
+          d="M224 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z"
+        />
+      </motion.svg>
     </motion.div>
   );
 };
