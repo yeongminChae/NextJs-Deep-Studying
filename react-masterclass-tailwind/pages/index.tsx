ec94bbb0270706975689ca078b4be403c09c1d22
Yeongmin_Chae <codudals98@naver.com>
2022-09-15 11:09:26 +0900

    Animation - Framer Motion : Motion Value part 3

--- react-masterclass-tailwind/pages/index.tsx
+++ react-masterclass-tailwind/pages/index.tsx
@@ -1,23 +1,38 @@
 import type { NextPage } from "next";
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import { motion, useMotionValue, useTransform, useScroll } from "framer-motion";
 import { useEffect } from "react";
 
 const Home: NextPage = () => {
   const x = useMotionValue(0);
-  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
-  useEffect(() => {
-    // x.onChange(()=>console.log(x.get()))
-    scale.onChange(() => console.log(scale.get()));
-  }, [x, scale]);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 0, 800],
+    [
+      "linear-gradient(135deg,rgb(0, 238, 214),rgb(18, 70, 240))",
+      "linear-gradient(135deg,rgb(238,0,153),rgb(221,0,238))",
+      "linear-gradient(135deg,rgb(34, 237, 44),rgb(173, 170, 27))",
+    ]
+  );
+  const { scrollY, scrollYProgress } = useScroll();
+  // useEffect(() => {
+  //   scrollY.onChange(() => {
+  //     console.log(scrollY.get(), scrollYProgress.get());
+  //   });
+  // }, [scrollY, scrollYProgress]);
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-gradient-to-tl from-purple-600 to-pink-600">
+    <motion.div
+      style={{ background: gradient }}
+      className="flex h-[200vh] w-screen items-center justify-center bg-gradient-to-tl from-purple-600 to-pink-600"
+    >
       <motion.div
         drag="x"
         dragSnapToOrigin
-        style={{ x, scale: scale }}
+        style={{ x, rotateZ, scale }}
         className="grid h-32 w-32 grid-cols-2 rounded-2xl bg-white shadow-md "
       />
-    </div>
+    </motion.div>
   );
 };
 
