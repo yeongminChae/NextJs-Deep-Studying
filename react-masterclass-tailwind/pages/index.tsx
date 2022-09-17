5435504b403a538fb31e5b9e9b8c0e24f3590a20
Yeongmin_Chae <codudals98@naver.com>
2022-09-17 12:46:26 +0900

    Animation - Framer Motion : Slider part 2 ( i have a error in this code, which when i press next after prev button animation is came from the wrong direction )

--- react-masterclass-tailwind/pages/index.tsx
+++ react-masterclass-tailwind/pages/index.tsx
@@ -3,53 +3,64 @@ import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 
 const box = {
-  invisible: {
-    x: 500,
+  entry: (isBack: boolean) => ({
+    x: isBack ? -500 : 500,
     opacity: 0,
     scale: 0,
-  },
-  visible: {
+  }),
+  center: {
     x: 0,
     opacity: 1,
     scale: 1,
     transition: {
-      duration: 1,
+      duration: 0.3,
     },
   },
-  exit: {
-    x: -500,
+  exit: (isBack: boolean) => ({
+    x: isBack ? 500 : -500,
     opacity: 0,
     scale: 0,
     rotateX: 180,
     transition: {
-      duration: 1,
+      duration: 0.3,
     },
-  },
+  }),
 };
 
 const Home: NextPage = () => {
   const [visible, setVisible] = useState(1);
-  const nextPls = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  const [back, setBack] = useState(false);
+  const nextPls = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prevPls = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
+  const customValue = {
+    direction: back,
+  };
   return (
     <motion.div className="flex h-screen w-screen flex-col items-center justify-center bg-gradient-to-tl from-purple-600 to-pink-600">
-      <AnimatePresence>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
-          i === visible ? (
-            <motion.div
-              key={i}
-              variants={box}
-              initial="invisible"
-              animate="visible"
-              exit="exit"
-              className="absolute flex h-40 w-40 items-center justify-center rounded-[40px] bg-white text-2xl shadow-xl"
-            >
-              {i}
-            </motion.div>
-          ) : null
-        )}
+      <AnimatePresence mode="wait" custom={customValue.direction}>
+        <motion.div
+          key={visible}
+          custom={customValue.direction}
+          variants={box}
+          initial="entry"
+          animate="center"
+          exit="exit"
+          className="absolute flex h-40 w-64 items-center justify-center rounded-[40px] bg-white text-2xl shadow-xl"
+        >
+          {visible}
+        </motion.div>
       </AnimatePresence>
       <button className="absolute bottom-52" onClick={nextPls}>
         Next
+      </button>
+      <button className="absolute bottom-48" onClick={prevPls}>
+        Prev
       </button>
     </motion.div>
   );
