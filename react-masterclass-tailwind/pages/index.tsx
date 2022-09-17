c9067e1ae2d08a5d5d3d00ea50f8d77e9bdd1c7d
Yeongmin_Chae <codudals98@naver.com>
2022-09-17 18:32:12 +0900

    Animation - Framer Motion : Final Project part 2

--- react-masterclass-tailwind/pages/index.tsx
+++ react-masterclass-tailwind/pages/index.tsx
@@ -2,34 +2,40 @@ import type { NextPage } from "next";
 import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 import { cls } from "../libs/client/utils";
+import styled from "styled-components";
+
+const Grid = styled.div`
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
 
 const Home: NextPage = () => {
-  const [clicked, setClicked] = useState(false);
-  const toggle = () => setClicked((prev) => !prev);
+  const [clickedId, setClickedId] = useState<null | string>(null);
   return (
-    <motion.div
-      onClick={toggle}
-      className="flex h-screen w-screen items-center justify-around bg-gradient-to-r from-[#c407d1] to-[#e09]"
-    >
-      <motion.div className="box-child grid w-[50vw] grid-cols-3 gap-3.5">
-        <motion.div
-          layoutId="Hello"
-          className="col-span-2 h-40 rounded-[40px] bg-white text-2xl shadow-xl"
-        />
-        <motion.div className="h-40 rounded-[40px] bg-white text-2xl shadow-xl" />
-        <motion.div className="h-40 rounded-[40px] bg-white text-2xl shadow-xl" />
-        <motion.div className="col-span-2 h-40 rounded-[40px] bg-white text-2xl shadow-xl" />
-      </motion.div>
-      <AnimatePresence>
-        {clicked ? (
+    <motion.div className="flex h-screen w-screen items-center justify-around bg-gradient-to-r from-[#c407d1] to-[#e09]">
+      <Grid className="grid w-[50vw] grid-cols-3 gap-3.5 ">
+        {["1", "2", "3", "4"].map((n) => (
           <motion.div
+            onClick={() => setClickedId(n)}
+            key={n}
+            layoutId={n} // cf) int + "" -> string
+            className="h-40 rounded-[40px] bg-white text-2xl shadow-xl"
+          />
+        ))}
+      </Grid>
+      <AnimatePresence>
+        {clickedId ? (
+          <motion.div
+            onClick={() => setClickedId(null)}
             initial={{ backgroundColor: "rgba(0,0,0,0)" }}
             animate={{ backgroundColor: "rgba(0,0,0,0.5)" }}
             exit={{ backgroundColor: "rgba(0,0,0,0)" }}
             className="absolute flex h-[100%] w-[100%] items-center justify-center "
           >
             <motion.div
-              layoutId="Hello"
+              layoutId={clickedId}
               className="h-[200px] w-[350px] rounded-[40px] bg-white text-2xl shadow-xl"
             />
           </motion.div>
