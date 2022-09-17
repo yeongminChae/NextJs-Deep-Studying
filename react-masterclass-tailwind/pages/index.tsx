db349e14b502cbd1eaac65c6da6fde37bd3f23f8
Yeongmin_Chae <codudals98@naver.com>
2022-09-17 16:08:35 +0900

    Animation - Framer Motion : "You need to watch this" < layout props with two box & two circle >

--- react-masterclass-tailwind/pages/index.tsx
+++ react-masterclass-tailwind/pages/index.tsx
@@ -9,18 +9,24 @@ const Home: NextPage = () => {
   return (
     <motion.div
       onClick={toggleClick}
-      className="flex h-screen w-screen items-center justify-center bg-gradient-to-tl from-purple-600 to-pink-600"
+      className="flex h-screen w-screen items-center justify-around bg-gradient-to-tl from-purple-600 to-pink-600"
     >
-      <motion.div
-        className={cls(
-          "flex h-64 w-64  rounded-[40px] bg-white text-2xl shadow-xl",
-          clicked ? "items-center justify-center" : "items-start justify-start "
-        )}
-      >
-        <motion.div
-          layout
-          className="h-[80px] w-[80px] rounded-full bg-[rgb(0,165,255)] shadow-xl"
-        ></motion.div>
+      <motion.div className="flex h-64 w-64 items-center justify-center rounded-[40px] bg-white text-2xl shadow-xl">
+        {!clicked ? (
+          <motion.div
+            layoutId="circle"
+            className="h-[80px] w-[80px] rounded-full bg-[rgb(0,165,255)] shadow-xl"
+          />
+        ) : null}
+      </motion.div>
+      <motion.div className="flex h-64 w-64 items-center justify-center rounded-[40px] bg-white text-2xl shadow-xl">
+        {clicked ? (
+          <motion.div
+            layoutId="circle"
+            className="h-[80px] w-[80px] rounded-md bg-[rgb(0,165,255)] shadow-xl"
+            style={{ scale: 2 }}
+          />
+        ) : null}
       </motion.div>
     </motion.div>
   );
