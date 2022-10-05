706f70b6ac4dfbe0b77f47d30c06a9deeb2002d7
Yeongmin_Chae <codudals98@naver.com>
2022-10-05 18:01:17 +0900

    Nomflix Home Screen part 2

--- react-masterclass-tailwind/libs/client/utils.ts
+++ react-masterclass-tailwind/libs/client/utils.ts
@@ -1,3 +1,7 @@
 export function cls(...classname: string[]) {
   return classname.join(" ");
 }
+
+export function makeImagePath(id: string, format?: string) {
+  return `https://image.tmdb.org/t/p/${format ? format : "original"}/${id}`;
+}
--- react-masterclass-tailwind/pages/nomflix/index.tsx
+++ react-masterclass-tailwind/pages/nomflix/index.tsx
@@ -1,15 +1,48 @@
 import { useQuery } from "@tanstack/react-query";
 import type { NextPage } from "next";
-import { getMovies } from "../api";
+import styled from "styled-components";
+import { makeImagePath } from "../../libs/client/utils";
+import { getMovies, IGetMoviesResult } from "../api";
 import Header from "./Components/Header";
 
+const Banner = styled.div<{ bgPhoto: string }>`
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgPhoto});
+`;
+
 const Home: NextPage = () => {
-  const { data, isLoading } = useQuery(["movies", "nowPlaying"], getMovies);
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
   console.log(data, isLoading);
   return (
-    <div className="h-[200vh] bg-neutral-400 ">
+    <div className="w-full ">
       <Header />
-      <div></div>
+      <div id="wraper" className="h-[200vh] bg-black  ">
+        {isLoading ? (
+          <div
+            id="loader"
+            className="flex h-[20vh] items-center justify-center  "
+          >
+            LOADING...{" "}
+          </div>
+        ) : (
+          <Banner
+            bgPhoto={makeImagePath(data?.results[5].backdrop_path || "")}
+            className="flex h-[100vh] flex-col justify-center bg-cover p-10 "
+          >
+            <div id="title" className="mb-4 text-4xl text-white">
+              {" "}
+              {data?.results[0].title}{" "}
+            </div>
+            <div id="overview" className="w-1/2 text-base text-white ">
+              {data?.results[0].overview}{" "}
+            </div>
+          </Banner>
+        )}{" "}
+        {/* ) : <></> }{" "} return Fragment : for returning many elements to next each other without single common parent*/}
+      </div>
     </div>
   );
 };
