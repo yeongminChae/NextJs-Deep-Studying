4e40266efcd5e9b37e25441515b5fe1508c479c7
Yeongmin_Chae <codudals98@naver.com>
2022-11-28 19:28:39 +0900

    new message

--- carrot-market/pages/enter.tsx
+++ carrot-market/pages/enter.tsx
@@ -64,12 +64,13 @@ const Enter: NextPage = () => {
   }, [tokenData, router]);
   return (
     <div className="mt-10 px-4">
-      <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
+      {/* <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3> */}
+      <h3 className="text-center text-3xl font-bold">new say hi</h3>
       <div className="mt-8">
         {data?.ok ? (
           <form
             onSubmit={tokenHandleSubmit(onTokenValid)}
-            className="flex flex-col mt-8 space-y-4"
+            className="mt-8 flex flex-col space-y-4"
           >
             <Input
               register={tokenRegister("token", {
@@ -85,13 +86,13 @@ const Enter: NextPage = () => {
         ) : (
           <>
             <div className="flex flex-col items-center">
-              <h5 className="text-sm text-gray-500 font-medium ">
+              <h5 className="text-sm font-medium text-gray-500 ">
                 Enter using:
               </h5>
-              <div className="grid border-b w-full mt-8 grid-cols-2 ">
+              <div className="mt-8 grid w-full grid-cols-2 border-b ">
                 <button
                   className={cls(
-                    "pb-4 font-medium text-sm border-b-2",
+                    "border-b-2 pb-4 text-sm font-medium",
                     method === "email"
                       ? "border-orange-500 text-orange-400"
                       : "border-transparent text-gray-500 "
@@ -102,7 +103,7 @@ const Enter: NextPage = () => {
                 </button>
                 <button
                   className={cls(
-                    "pb-4 font-medium text-sm border-b-2",
+                    "border-b-2 pb-4 text-sm font-medium",
                     method === "phone"
                       ? "border-orange-500 text-orange-400"
                       : "border-transparent text-gray-500 "
@@ -115,7 +116,7 @@ const Enter: NextPage = () => {
             </div>
             <form
               onSubmit={handleSubmit(onValid)}
-              className="flex flex-col mt-8 space-y-4"
+              className="mt-8 flex flex-col space-y-4"
             >
               <label
                 htmlFor="input"
@@ -168,10 +169,10 @@ const Enter: NextPage = () => {
               </span>
             </div>
           </div>
-          <div className="grid grid-cols-2 gap-3 mt-2">
-            <button className="flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-100">
+          <div className="mt-2 grid grid-cols-2 gap-3">
+            <button className="flex items-center justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-100">
               <svg
-                className="w-5 h-5"
+                className="h-5 w-5"
                 aria-hidden="true"
                 fill="currentColor"
                 viewBox="0 0 20 20"
@@ -179,9 +180,9 @@ const Enter: NextPage = () => {
                 <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
               </svg>
             </button>
-            <button className="flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-100">
+            <button className="flex items-center justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-100">
               <svg
-                className="w-5 h-5"
+                className="h-5 w-5"
                 aria-hidden="true"
                 fill="currentColor"
                 viewBox="0 0 20 20"
